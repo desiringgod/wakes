61868195ce8b650043a2b3c9f07417b90abaa120
Ben Hutton <benhutton@gmail.com>
2015-09-29 09:55:39 -0400

    Inject migrations into the host app

--- lib/wakes/engine.rb
+++ lib/wakes/engine.rb
@@ -6,5 +6,13 @@ module Wakes
       g.assets false
       g.helper false
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
