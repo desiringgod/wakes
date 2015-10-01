965758843913b81f0982d5819281424953cb4b03
Ben Hutton <benhutton@gmail.com>
2015-10-01 16:40:30 -0400

    Add wakes logger

--- lib/wakes.rb
+++ lib/wakes.rb
@@ -3,6 +3,14 @@ require "wakes/version"
 require "wakes/engine"
 
 module Wakes
+  def self.logger
+    @logger || Rails.logger
+  end
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
   def self.table_name_prefix
     'wakes_'
   end
