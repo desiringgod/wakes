ff6dbda10c2fc2822d3820249767037a0910ce7a
Ben Hutton <benhutton@gmail.com>
2016-06-30 12:38:00 -0400

    Make wakes matcher more robust

--- lib/wakes/matchers.rb
+++ lib/wakes/matchers.rb
@@ -9,9 +9,11 @@ RSpec::Matchers.define :have_wakes_graph do |canonical_location:, legacy_locatio
     expected_location_count = legacy_locations.size
     expected_location_count += 1 if canonical_location.present?
 
+    canonical_location_uri = URIFromLocationString.generate(canonical_location)
+
     wakes_resource.locations.count == expected_location_count &&
-      wakes_resource.canonical_location.host == URIFromLocationString.generate(canonical_location).host &&
-      wakes_resource.canonical_location.path == URIFromLocationString.generate(canonical_location).request_uri &&
+      wakes_resource.canonical_location.try(:host) == canonical_location_uri.try(:host) &&
+      wakes_resource.canonical_location.try(:path) == canonical_location_uri.try(:request_uri) &&
       wakes_resource.legacy_locations.pluck(:host, :path).sort ==
         legacy_locations.map { |x| URIFromLocationString.get_host_and_path(x) }.sort
   end
@@ -19,7 +21,7 @@ RSpec::Matchers.define :have_wakes_graph do |canonical_location:, legacy_locatio
   failure_message do |wakes_resource|
     legacy_locations_array = legacy_locations.map { |x| URIFromLocationString.get_host_and_path(x) }.sort
     message = "Expected canonical location \"#{canonical_location}\","
-    message += " got canonical location \"#{wakes_resource.canonical_location.label}\"\n"
+    message += " got canonical location \"#{wakes_resource.canonical_location.try(:label)}\"\n"
     message += "Expected legacy locations #{legacy_locations_array}, "
     message + "got legacy locations #{wakes_resource.legacy_locations.pluck(:host, :path).sort}"
   end
