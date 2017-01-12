77c7addd0d7932a422a60720c1688e425f3d2858
Joe Osburn <joe@jnodev.com>
2017-01-11 18:15:17 -0600

    Create GoogleAnalyticsApiWrapper#get_page_of_pageviews

--- app/wrappers/wakes/google_analytics_api_wrapper.rb
+++ app/wrappers/wakes/google_analytics_api_wrapper.rb
@@ -4,6 +4,7 @@ require 'google/apis/analytics_v3'
 
 class Wakes::GoogleAnalyticsApiWrapper
   AUTHORIZATION_SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
+  PAGE_SIZE = 1_000
 
   # More precisely, this sums up the pageviews for this particular path and
   # any other paths with query strings
@@ -19,7 +20,44 @@ class Wakes::GoogleAnalyticsApiWrapper
     ).totals_for_all_results['ga:pageviews'].to_i
   end
 
+  def get_page_of_pageviews(page, start_date:, end_date:, profile_id: Wakes.configuration.ga_profiles['default'])
+    results = authorized_analytics_service.get_ga_data(
+      "ga:#{profile_id}",
+      format_date(start_date),
+      format_date(end_date),
+      'ga:pageviews',
+      :dimensions => 'ga:pagePath',
+      :sort => '-ga:pageviews',
+      :start_index => start_index_for_page(page)
+    )
+    create_page(results.rows || [], results.rows.count < results.items_per_page)
+  end
+
   private
+
+  def format_date(date)
+    date.to_date.to_s
+  end
+
+  def create_page(rows, end_of_page)
+    Page.new(page_views_from_rows(rows), end_of_page)
+  end
+
+  def page_views_from_rows(rows)
+    rows.map { |row| create_page_views(row) }.compact
+  end
+
+  def create_page_views(row)
+    PageViews.new(Url.new(row[0]), row[1].to_i)
+  rescue URI::InvalidURIError
+    # Ignore invalid URIs, we won't being doing lookups
+    puts "Invalid path, not attempting to lookup: #{row[0]}".red
+  end
+
+  # indexes start at 1, so page 1 would = 1, page 2 would = 1001, page 3 would = 2001, etc.
+  def start_index_for_page(page)
+    (PAGE_SIZE * page) - (PAGE_SIZE - 1)
+  end
 
   def authorized_analytics_service
     @service ||= Google::Apis::AnalyticsV3::AnalyticsService.new.tap do |analytics|
@@ -29,6 +67,66 @@ class Wakes::GoogleAnalyticsApiWrapper
 
   def credentials
     @credentials ||= Google::Auth::ServiceAccountCredentials.from_env(AUTHORIZATION_SCOPE)
+  end
+
+  Page = Struct.new(:rows, :end?)
+
+  class PageViews
+    attr_reader :count, :url
+
+    def initialize(url, count)
+      @url = url
+      @count = count
+    end
+
+    def to_s
+      "#{url}: #{count}"
+    end
+  end
+
+  class Url
+    attr_reader :uri
+    delegate :query, :path, :to => :uri
+
+    def initialize(uri)
+      @uri = URI.parse(uri)
+    end
+
+    def to_s
+      uri.to_s
+    end
+
+    def params
+      @params ||= CGI.parse(query || '')
+    end
+
+    def lang
+      return @lang if defined?(@lang)
+      @lang = params.include?('lang') ? params['lang'][0] : nil
+    end
+
+    def extension
+      return @extension if defined?(@extension)
+      @extension = File.extname(path).delete('.').presence
+    end
+
+    def sanitized_path
+      @sanitized_path ||= path_without_extension + extension_if_not_html + lang_if_not_en
+    end
+
+    private
+
+    def path_without_extension
+      path.rpartition('.').first.presence || path
+    end
+
+    def extension_if_not_html
+      extension.present? && extension != 'html' ? ".#{extension}" : ''
+    end
+
+    def lang_if_not_en
+      lang.present? && lang != 'en' ? "?lang=#{lang}" : ''
+    end
   end
 
   class PrepareFiltersForGAPagePath
--- spec/wrappers/wakes/google_analytics_api_wrapper_spec.rb
+++ spec/wrappers/wakes/google_analytics_api_wrapper_spec.rb
@@ -38,6 +38,180 @@ RSpec.describe Wakes::GoogleAnalyticsApiWrapper do
       end
     end
   end
+
+  describe '#get_page_of_pageviews' do
+    let(:rows) { [['/path', 151], ['/path/2', 500]] }
+    let(:results) { double('Results', :rows => rows, :items_per_page => 2) }
+    let(:analytics_service) { double('AnalyticsService', :get_ga_data => results) }
+    let(:start_date) { Time.new(2017, 10, 10).to_date }
+    let(:end_date) { Time.new(2017, 12, 12).to_date }
+
+    before do
+      allow(subject).to receive(:authorized_analytics_service).and_return(analytics_service)
+    end
+
+    it 'calls #get_ga_data on GA' do
+      expect(analytics_service).to receive(:get_ga_data).with(
+        'ga:profile',
+        '2017-10-10',
+        '2017-12-12',
+        'ga:pageviews',
+        :dimensions => 'ga:pagePath',
+        :sort => '-ga:pageviews',
+        :start_index => 1
+      )
+      subject.get_page_of_pageviews(1, :start_date => start_date, :end_date => end_date, :profile_id => 'profile')
+    end
+
+    it 'returns a Page of data' do
+      page = subject.get_page_of_pageviews(1, :start_date => start_date, :end_date => end_date)
+      rows = page.rows
+      expect(rows[0].url.path).to eq('/path')
+      expect(rows[0].count).to eq(151)
+      expect(rows[1].url.path).to eq('/path/2')
+      expect(rows[1].count).to eq(500)
+      expect(page).to_not be_end
+    end
+
+    context 'page is not 1' do
+      it 'correctly calculates the new start index' do
+        expect(analytics_service).to receive(:get_ga_data).with(
+          any_args,
+          a_hash_including(:start_index => 2001)
+        )
+        subject.get_page_of_pageviews(3, :start_date => start_date, :end_date => end_date)
+      end
+    end
+
+    context 'rows size is less than items_per_page' do
+      before { allow(results).to receive(:items_per_page).and_return(3) }
+
+      it 'sets the page end to true' do
+        page = subject.get_page_of_pageviews(1, :start_date => start_date, :end_date => end_date)
+        expect(page).to be_end
+      end
+    end
+  end
+end
+
+RSpec.describe Wakes::GoogleAnalyticsApiWrapper::Url do
+  describe '#extension' do
+    context 'extension is present' do
+      subject { described_class.new('/authors/john-piper.html') }
+
+      it 'returns the extension' do
+        expect(subject.extension).to eq('html')
+      end
+    end
+
+    context 'extension is not present' do
+      subject { described_class.new('/authors/john-piper') }
+
+      it 'returns nil' do
+        expect(subject.extension).to be(nil)
+      end
+    end
+  end
+
+  describe '#lang' do
+    context 'lang param is present' do
+      subject { described_class.new('/authors/john-piper/messages?page=2&lang=es&sort=newest') }
+
+      it 'returns the value' do
+        expect(subject.lang).to eq('es')
+      end
+    end
+
+    context 'lang param is not present' do
+      subject { described_class.new('/authors/john-piper/messages?page=2&sort=newest') }
+
+      it 'returns nil' do
+        expect(subject.lang).to be(nil)
+      end
+    end
+  end
+
+  describe '#sanitized_path' do
+    context 'params and extension not present' do
+      subject { described_class.new('/authors/john-piper') }
+
+      it 'returns the path' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper')
+      end
+    end
+
+    context 'params not present and extension present but html' do
+      subject { described_class.new('/authors/john-piper.html') }
+
+      it 'returns the path' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper')
+      end
+    end
+
+    context 'params not present and extension present and not html' do
+      subject { described_class.new('/authors/john-piper.rss') }
+
+      it 'returns the path with extension' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper.rss')
+      end
+    end
+
+    context 'params present no lang and extension not present' do
+      subject { described_class.new('/authors/john-piper/messages?page=2&sort=newest') }
+
+      it 'returns the path' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages')
+      end
+    end
+
+    context 'params present with lang not en and extension not present' do
+      subject { described_class.new('/authors/john-piper/messages?page=2&lang=es&sort=newest') }
+
+      it 'returns the path with lang param' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages?lang=es')
+      end
+    end
+
+    context 'params present with lang en and extension not present' do
+      subject { described_class.new('/authors/john-piper/messages?page=2&lang=en&sort=newest') }
+
+      it 'returns the path' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages')
+      end
+    end
+
+    context 'params present with lang not en and extension present but html' do
+      subject { described_class.new('/authors/john-piper/messages.html?page=2&lang=es&sort=newest') }
+
+      it 'returns the path with lang param' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages?lang=es')
+      end
+    end
+
+    context 'params present with lang not en and extension present and not html' do
+      subject { described_class.new('/authors/john-piper/messages.rss?page=2&lang=es&sort=newest') }
+
+      it 'returns the path with lang param and extension' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages.rss?lang=es')
+      end
+    end
+
+    context 'params present with lang en and extension present but html' do
+      subject { described_class.new('/authors/john-piper/messages.html?page=2&lang=en&sort=newest') }
+
+      it 'returns the path' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages')
+      end
+    end
+
+    context 'params present with lang en and extension present and not html' do
+      subject { described_class.new('/authors/john-piper/messages.rss?page=2&lang=en&sort=newest') }
+
+      it 'returns the path with extension' do
+        expect(subject.sanitized_path).to eq('/authors/john-piper/messages.rss')
+      end
+    end
+  end
 end
 
 RSpec.describe Wakes::GoogleAnalyticsApiWrapper::PrepareFiltersForGAPagePath do
