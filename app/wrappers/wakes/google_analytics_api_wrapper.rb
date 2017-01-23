905a2850e6e9758e53f1e898a54c9f8b28bdb32a
Joe Osburn <joe+desiringgod@jnodev.com>
2017-01-23 15:38:25 -0600

    Remove old PageviewCountUpdaterService, remove old GoogelAnalyticsApiWrapper#get_pageviews_for_path method, remove old GoogleAnalyticsPageviewJob

--- app/wrappers/wakes/google_analytics_api_wrapper.rb
+++ app/wrappers/wakes/google_analytics_api_wrapper.rb
@@ -6,20 +6,6 @@ class Wakes::GoogleAnalyticsApiWrapper
   AUTHORIZATION_SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
   PAGE_SIZE = 1_000
 
-  # More precisely, this sums up the pageviews for this particular path and
-  # any other paths with query strings
-  def get_pageviews_for_path(path, start_date:, end_date:, profile_id: Wakes.configuration.ga_profiles['default'])
-    authorized_analytics_service.get_ga_data(
-      "ga:#{profile_id}",
-      format_date(start_date),
-      format_date(end_date),
-      'ga:pageviews',
-      :dimensions => 'ga:pagePath',
-      :filters => PrepareFiltersForGAPagePath.new(path).filters,
-      :sort => '-ga:pageviews'
-    ).totals_for_all_results['ga:pageviews'].to_i
-  end
-
   def get_page_of_pageviews(page, start_date:, end_date:, profile_id: Wakes.configuration.ga_profiles['default'])
     results = authorized_analytics_service.get_ga_data(
       "ga:#{profile_id}",
@@ -127,58 +113,5 @@ class Wakes::GoogleAnalyticsApiWrapper
     def lang_if_not_en
       lang.present? && lang != 'en' ? "?lang=#{lang}" : ''
     end
-  end
-
-  class PrepareFiltersForGAPagePath
-    attr_accessor :path
-
-    def initialize(path)
-      @path = path
-    end
-
-    def filters
-      str = "ga:pagePath=~#{regexp_string_for_path}"
-      str += ';ga:pagePath!@lang=' unless lang_included?
-      str
-    end
-
-    # This is a string written to match exact path and any params, but no subdirectories.
-    # So for the /blog path, it would match /blog and /blog?page=1 but not /blog/posts/asdf
-    def regexp_string_for_path
-      str = "^#{quoted_path}(\\?|$)"
-      str.length > 128 ? limit_regexp_string(str) : str
-    end
-
-    private
-
-    # rubocop:disable Metrics/LineLength
-    # This method replaces appropriate number of characters in the beginning of the last segment
-    # with a .* to limit the regexp string to 128 characters.
-    # This helps avoid Google analytics' complaints.
-    # Example /interviews/if-our-sins-are-punished-by-eternal-separation-from-god-why-did-jesus-only-have-to-suffer-momentary-separation
-    # will become ^/interviews/.*our-sins-are-punished-by-eternal-separation-from-god-why-did-jesus-only-have-to-suffer-momentary-separation(\\?|$)
-    # rubocop:enable Metrics/LineLength
-    def limit_regexp_string(str)
-      difference = str.length - 126 # we are adding 2 characters .* 126 helps us get that.
-      path_segments = quoted_path.split('/')
-      last_segment = path_segments.pop
-      last_segment.slice!(0..difference - 1)
-      path_segments.push '.*' + last_segment
-      "^#{path_segments.join('/')}(\\?|$)"
-    end
-
-    # This escapes special characters in a string to prepare it for a regular expression
-    # It undoes quoting the '-' because unquoting them doesn't seem to affect the result
-    def quoted_path
-      Regexp.quote(path).gsub('\\-', '-')
-    end
-
-    def lang_included?
-      /lang=/ =~ path
-    end
-  end
-
-  def format_date(date)
-    date.to_date.to_s
   end
 end
--- spec/wrappers/wakes/google_analytics_api_wrapper_spec.rb
+++ spec/wrappers/wakes/google_analytics_api_wrapper_spec.rb
@@ -2,43 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe Wakes::GoogleAnalyticsApiWrapper do
-  # NOTE these tests will need a Google API TOKEN to pass
-  # Remember to remove the API TOKEN before committing!
-  # before do
-  #   ENV['GOOGLE_PRIVATE_KEY'] = '<google-private-key>'
-  #   ENV['GOOGLE_CLIENT_EMAIL'] = '<google-client-email>'
-  #   ENV['GOOGLE_ANALYTICS_PROFILE_ID'] = '<google-analytics-profile-id>'
-  # end
-
-  describe '#get_pageviews_for_path' do
-    # Describes this view: https://www.google.com/analytics/web/?hl=en#report/content-pages/a1853263w3269662p8231065/%3F_u.date00%3D20140101%26_u.date01%3D20141231%26explorer-table.plotKeys%3D%5B%5D%26explorer-table.advFilter%3D%5B%5B0%2C%22analytics.pagePath%22%2C%22RE%22%2C%22%5E%2Fabout(%5C%5C%3F%7C%24)%22%2C0%5D%5D%26explorer-table.rowCount%3D100/
-    it 'returns the total pageviews for the given date range, converting date input to date strings' do
-      pageviews = subject.get_pageviews_for_path('/about',
-                                                 :start_date => '2014-01-01T00:00:00+00:00',
-                                                 :end_date => '2014-12-31T00:00:00+00:00')
-
-      expect(pageviews).to eq 78_891
-    end
-
-    describe 'handles lang params' do
-      it 'only matches if original path has lang' do
-        pageviews = subject.get_pageviews_for_path('/sermons/for-his-sake-and-for-your-joy-go-low?lang=es',
-                                                   :start_date => '2015-06-01',
-                                                   :end_date => '2015-07-10')
-
-        expect(pageviews).to eq 104
-      end
-
-      it 'does not match if original path does not have lang' do
-        pageviews = subject.get_pageviews_for_path('/sermons/for-his-sake-and-for-your-joy-go-low',
-                                                   :start_date => '2015-06-01',
-                                                   :end_date => '2015-07-10')
-
-        expect(pageviews).to eq 1114
-      end
-    end
-  end
-
   describe '#get_page_of_pageviews' do
     let(:rows) { [['/path', 151], ['/path/2', 500]] }
     let(:results) { double('Results', :rows => rows, :items_per_page => 2) }
@@ -211,62 +174,5 @@ RSpec.describe Wakes::GoogleAnalyticsApiWrapper::Url do
         expect(subject.sanitized_path).to eq('/authors/john-piper/messages.rss')
       end
     end
-  end
-end
-
-RSpec.describe Wakes::GoogleAnalyticsApiWrapper::PrepareFiltersForGAPagePath do
-  describe '#filters' do
-    it 'generates the required filter for a path' do
-      subject = described_class.new('/blog')
-      expect(subject.filters).to include('ga:pagePath=~^/blog(\\?|$)')
-    end
-
-    describe 'lang parameter sensitivity' do
-      it 'appends filter to exclude lang parameter by default' do
-        subject = described_class.new('/sermons/for-his-sake-and-for-your-joy-go-low?lang=es')
-        expect(subject.filters).to_not end_with(';ga:pagePath!@lang=')
-      end
-
-      it 'does not exclude lang parameter if it is a part of the path' do
-        subject = described_class.new('/sermons/for-his-sake-and-for-your-joy-go-low')
-        expect(subject.filters).to end_with(';ga:pagePath!@lang=')
-      end
-    end
-  end
-
-  describe '#regexp_string_for_path' do
-    let(:regexp_string) { described_class.new('/blog').regexp_string_for_path }
-    subject { Regexp.new(regexp_string) }
-
-    it 'matches /blog,' do
-      is_expected.to match('/blog')
-    end
-
-    it 'matches /blog?page=1' do
-      is_expected.to match('/blog?page=1')
-    end
-
-    it 'does not match /blog/posts/asdf' do
-      is_expected.to_not match('/blog/posts/asdf')
-    end
-
-    # rubocop:disable Metrics/LineLength
-    context 'regular expressions which are longer than 128 characters by default' do
-      subject { described_class.new('/interviews/if-our-sins-are-punished-by-eternal-separation-from-god-why-did-jesus-only-have-to-suffer-momentary-separation') }
-
-      it 'limits them to 128 characters' do
-        expect(subject.regexp_string_for_path.length).to eq 128
-      end
-
-      it 'matches the url' do
-        regexp = Regexp.new(subject.regexp_string_for_path)
-        expect(regexp).to match '/interviews/if-our-sins-are-punished-by-eternal-separation-from-god-why-did-jesus-only-have-to-suffer-momentary-separation'
-      end
-
-      it 'replaces appropriate number of characters in the beginning part of the last segment with a .*' do
-        expect(subject.regexp_string_for_path).to eq '^/interviews/.*our-sins-are-punished-by-eternal-separation-from-god-why-did-jesus-only-have-to-suffer-momentary-separation(\\?|$)'
-      end
-    end
-    # rubocop:enable Metrics/LineLength
   end
 end
