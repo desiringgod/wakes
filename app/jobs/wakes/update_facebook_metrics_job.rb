f62e2c2aed39cf7c12fd0a42298839d85d30f446
Gagan Awhad <gagan.a.awhad@gmail.com>
2022-05-16 10:38:58 -0400

    Remove facebook

--- app/jobs/wakes/update_facebook_metrics_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Wakes::UpdateFacebookMetricsJob < ActiveJob::Base
-  queue_as :wakes_facebook_metrics
-
-  def perform(locations)
-    Wakes::FacebookCountUpdaterService.new(locations).update_facebook_count
-  end
-end
